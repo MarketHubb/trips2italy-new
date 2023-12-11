1ba29ddd16d14e415cc578e70d927b6798b61787
MarketHubb <chris@markethubb.com>
2023-12-11 14:48:00 -0600

    New: Add GTM tracking script

--- wp-content/themes/bootscore-child-main/header.php
+++ wp-content/themes/bootscore-child-main/header.php
@@ -28,6 +28,17 @@
     <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg" color="#0d6efd">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
+
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KBB6BZNM');</script>
+    <!-- End Google Tag Manager -->
+
+
     <?php wp_head(); ?>
 
     <?php $_SERVER['REFERER_ID'] = (get_the_ID() !== 28484) ? get_the_ID() : null; ?>
@@ -35,6 +46,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBB6BZNM"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
 <?php wp_body_open(); ?>
 
