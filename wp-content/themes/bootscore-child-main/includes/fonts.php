6b336b372446892d960eb1faf0347c8cc2d209d5
Chris Hubbard <chris@markethubb.com>
2024-09-30 11:32:39 -0500

    update: enqueue custom fonts.css file using adobe web fonts

--- wp-content/themes/bootscore-child-main/includes/fonts.php
+++ wp-content/themes/bootscore-child-main/includes/fonts.php
@@ -1,4 +1,12 @@
 <?php 
+/*
+Enqueue custom fonts
+*/
+function enqueue_custom_fonts() {
+    wp_enqueue_style('custom-fonts', get_stylesheet_directory_uri() . '/css/fonts.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_fonts');
+
 function enqueue_adobe_fonts() {
     wp_enqueue_style('adobe-fonts', 'https://use.typekit.net/ncd8wmg.css', array(), null);
 }
