a03f2db81f18057f1f11bc00a89497f1dace4acf
MarketHubb <chris@markethubb.com>
2025-01-09 09:39:22 -0600

    Fix: Update hero section for nested array arg for background image

--- wp-content/themes/bootscore-child-main/template-parts/tw-hero/content-hero-overlay-wave.php
+++ wp-content/themes/bootscore-child-main/template-parts/tw-hero/content-hero-overlay-wave.php
@@ -14,21 +14,21 @@ if (isset($args)) {
 <?php if (isset($hero_inputs) && !empty($hero_inputs)) { ?>
 
    <header class="block">
-      <div class="min-h-96 relative flex items-center bg-cover bg-center p-0 overflow-hidden" style="background-image: url(<?php echo $hero_inputs['images']['background_image']; ?>);">
+      <div class="min-h-96 relative flex items-center bg-cover bg-center p-0 overflow-hidden" style="background-image: url(<?php echo $hero_inputs['images']['background_image']['url']; ?>);">
          <span class="absolute inset-0 w-full h-full  bg-gradient-to-r from-[#121821] from-[100%] opacity-60 sm:opacity-100 sm:from-[#0d1117] sm:from-[1%] "></span>
          <!-- Copy -->
          <div class="md:pl-8 max-w-7xl mx-auto w-full z-10 relative px-6 md:px-0 py-24 lg:py-44 xl:py-56 ">
-            <div class="lg:max-w-[50%]">
+            <div class="lg:max-w-[62%]">
                <?php if (!empty($hero_inputs['breadcrumbs'])) { ?>
 
                   <?php get_template_part( 'template-parts/tw-hero/content', 'breadcrumbs', $hero_inputs['breadcrumbs'] ); ?>
                   
                <?php  } ?>
                <h1 class="text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-normal sm:leading-none">
-                  <span class="text-secondary-500 tracking-tight md:tracking-normal uppercase font-bold block leading-none"><?php echo $hero_inputs['copy']['heading_1']['desktop']; ?></span>
-                  <span class="text-[120%] text-white stylized leading-[1]"><?php echo $hero_inputs['copy']['heading_2']['desktop']; ?></span>
+                  <span class="text-secondary-500 tracking-tight md:tracking-normal font-bold block leading-none pb-4 "><?php echo $hero_inputs['copy']['heading_1']['desktop']; ?></span>
+                  <span class="text-[115%] text-white stylized leading-[1]"><?php echo $hero_inputs['copy']['heading_2']['desktop']; ?></span>
                </h1>
-               <p class="text-white text-base md:text-lg font-semibold antialiased mt-8 lg:max-w-[90%]">
+               <p class="text-white !leading-relaxed text-base md:text-[1.2rem] font-semibold antialiased mt-8 lg:max-w-[60%]">
                   <?php echo $hero_inputs['copy']['description']['desktop']; ?>
                </p>
             </div>
