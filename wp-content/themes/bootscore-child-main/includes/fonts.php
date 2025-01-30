ab4c9399821c1cbf1e14a726c341321a30dab361
MarketHubb <chris@markethubb.com>
2025-01-29 20:13:48 -0600

    Update: Remove unused Google Fonts calls

--- wp-content/themes/bootscore-child-main/includes/fonts.php
+++ wp-content/themes/bootscore-child-main/includes/fonts.php
@@ -10,5 +10,7 @@ add_action('wp_enqueue_scripts', 'enqueue_custom_fonts');
 function enqueue_project_web_fonts() {
     wp_enqueue_style('adobe-fonts', 'https://use.typekit.net/ncd8wmg.css', array(), null);
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap', array(), null);
+    // wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap', array(), null);
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_project_web_fonts');
